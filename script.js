a9bcfc04285e7aeb5bd9ef8a1b8fea962b9b68c8
ChainedMj <50352495+ChainedMj@users.noreply.github.com>
2020-08-13 19:08:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+
+
+function titleClickHandler(event){
+  console.log('Link was clicked!');
+  event.preventDefault();
+
+  const activelink = document.querySelector('.titles a.active');
+  if (activelink) activelink.classList.remove('active');
+
+  this.classList.add('active');
+  /* remove class 'active' from all articles */
+  const activearticle = document.querySelector('.post.active');
+  activearticle.classList.remove('active');
+  /* get 'href' attribute from the clicked link */
+  const articleid = this.getAttribute('href');
+  /* find the correct article using the selector (value of 'href' attribute) */
+  const newarticle = document.querySelector(articleid);
+  /* add class 'active' to the correct article */
+  newarticle.classList.add('active');
+}
+
+
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+  console.log('link generated');
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML='';
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+  for( let article of articles){
+    const articleId = article.getAttribute ('id');
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    html = html + linkHTML;
+  
+  }
+  titleList.innerHTML = html;
+  const links = document.querySelectorAll('.titles a');
+
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
+}
+
+generateTitleLinks();
+
+
